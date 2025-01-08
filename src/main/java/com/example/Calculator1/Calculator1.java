5f2cb02081bf6e6d065e14331579f75819425fd8
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-01-08 22:41:00 +0900

    Refactor: 리팩토링

--- src/main/java/com/example/Calculator1/Calculator1.java
+++ src/main/java/com/example/Calculator1/Calculator1.java
@@ -3,7 +3,9 @@ package com.example.Calculator1;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+
 public class Calculator1 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -14,26 +16,7 @@ public class Calculator1 {
 
             String operator = getOperator(sc);
 
-            double result = 0;
-            switch (operator) {
-                case "+":
-                    result = num1 + num2;
-                    break;
-                case "-":
-                    result = num1 - num2;
-                    break;
-                case "*":
-                    result = num1 * num2;
-                    break;
-                case "/":
-                    while (num2 == 0) {
-                        num2 = getInput("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. \n두번째 정수를 다시 입력하세요.", sc);
-                    }
-                    result = (double) num1 / num2;
-                    break;
-                default:
-                    break;
-            }
+            double result = calculate(sc, operator, num1, num2);
 
             System.out.println("계산 결과 : " + num1 + " " + operator + " " + num2 + " = " + result);
 
@@ -46,33 +29,50 @@ public class Calculator1 {
 
     // 입력값 받는 메소드
     private static int getInput(String prompt, Scanner sc) {
-        int num;
         while (true) {
             try {
                 System.out.println(prompt);
-                num = sc.nextInt();
-                break;
+                return sc.nextInt();
             } catch (InputMismatchException e) {
                 System.out.println("유효하지 않은 입력값 입니다. \n다시 입력해 주세요.");
                 sc.next();
             }
         }
-        return num;
     }
 
     // 연산기호 받는 메소드
     private static String getOperator(Scanner sc) {
-        String operator;
         while (true) {
             System.out.println("사칙 연산 기호를 입력하세요");
-            operator = sc.next();
-            if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) {
-                break;
+            String operator = sc.next();
+            if (checkOperator(operator)) {
+                return operator;
             } else {
                 System.out.println("잘못된 사칙연산 기호를 입력했습니다.\n다시 기호를 입력하세요.");
             }
         }
-        return operator;
+    }
+
+    private static boolean checkOperator (String operator) {
+        return operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/");
+    }
+
+    private static double calculate(Scanner sc,String operator, int num1, int num2) {
+        switch (operator) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            case "/":
+                while (num2 == 0) {
+                    num2 = getInput("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. \n두번째 정수를 다시 입력하세요.", sc);
+                }
+                return (double) num1 / num2;
+            default:
+                return 0;
+        }
     }
 }
 
